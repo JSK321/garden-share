9b375415596841f8ee6e3ee42150caf23ae3965b
Elijah B <69051669+elijah415hz@users.noreply.github.com>
2020-10-19 13:57:38 -0700

    basic file structure

---
+++ controllers/controller.js
@@ -0,0 +1,35 @@
+// *********************************************************************************
+// controller.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+
+// Requiring our models
+var db = require("../models");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // Get route
+  app.get("/", function(req, res) {
+
+  });
+
+
+  // POST route 
+  app.post("/", function(req, res) {
+
+  });
+
+  // DELETE route 
+  app.delete("/", function(req, res) {
+
+  });
+
+  // PUT route
+  app.put("/api/posts", function(req, res) {
+
+  });
+};
