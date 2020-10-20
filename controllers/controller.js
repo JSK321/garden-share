b92126c1ebb86710aa013f8898d2d371755bbcd4
ckomodo <chrissakwa@gmail.com>
2020-10-20 14:14:47 -0700

    nodemailer email set up

--- controllers/controller.js
+++ controllers/controller.js
@@ -7,31 +7,62 @@
 
 // Requiring our models
 var db = require("../models");
+var nodemailer = require("nodemailer");
 
 // Routes
 // =============================================================
-module.exports = function(app) {
-
+module.exports = function (app) {
   // Get route
-  app.get("/", function(req, res) {
-    res.render("index").end()
+  app.get("/", function (req, res) {
+    res.render("index").end();
   });
 
+  // POST route
+  app.post("/email", function (req, res) {
+    db.Owner.findOne({
+      where: {
+        id: req.body.userId,
 
-  // POST route 
-  app.post("/:email", function(req, res) {
-    const userType = req.body.userType,
-    const userId = req.body.userId,
-    const emailBody = req.body.emailBody
+      },
+    }).then(function (data) {
+      //email addresses
+
+      const newEmail = {
+        from: "patchedapp@gmail.com", //can we get from DB using ID?
+        to: "chrissakwa@gmail.com", //can we get from DB using ID?
+        subject: "testing nodemailer",
+        text: "this works as is",
+      };
+
+      //transportation module
+
+      const transport = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: "patchedapp@gmail.com",
+          pass: "Project2!",
+        },
       });
 
-  // DELETE route 
-  app.delete("/", function(req, res) {
-
+      //email sending
+      transport.sendMail(newEmail, (err, info) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("email sent successfully" + info);
+        }
+      });
+    });
+    res.json(data);
   });
+    // const userType = req.body.userType,
+    // const userId = req.body.userId,
+    // const emailBody = req.body.emailBody
+
+  // DELETE route
+  app.delete("/", function (req, res) {});
 
   // PUT route
-  app.put("/api/posts", function(req, res) {
+  app.put("/api/posts", function (req, res) {});
 
-  });
 };
