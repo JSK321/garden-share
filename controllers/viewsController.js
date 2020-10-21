7f6f3a4a31d4cc0708bd14fa04502beeeb0b7043
Elijah B <69051669+elijah415hz@users.noreply.github.com>
2020-10-21 14:00:22 -0700

    map get route created

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../models");
-const { route } = require("./ownerController");
 
 // HTML routes =================================
 // return index.handebars
@@ -10,25 +9,47 @@ router.get("/", function (req, res) {
 });
 
 // return profile.handlebars
-router.get("/profile", function(req,res){
+router.get("/profile", function (req, res) {
     res.render("profile")
 });
 
 // return profile.handlebars by id
 router.get("/profile/:id", function (req, res) {
-    db.Owner.findOne({where: {id: req.params.id}}).then(result=>{
+    db.Owner.findOne({ where: { id: req.params.id } }).then(result => {
         console.log(result.toJSON())
         res.render("profile", result.toJSON())
-    }).catch(err=>{
+    }).catch(err => {
         res.status(500).send(err);
     })
 });
 // return signup.handlebars for owners
 router.get("/owners/signup", function (req, res) {
-    res.render("signup", {route: "/api/owners"})
+    res.render("signup", { route: "/api/owners" })
 });
 // return signup.handlebars for gardeners
 router.get("/gardeners/signup", function (req, res) {
-    res.render("signup", {route: "/api/gardeners"})
+    res.render("signup", { route: "/api/gardeners" })
 });
+// return map.handlebars
+router.get("/map/:id", function (req, res) {
+    db.Gardener.findOne({ where: { id: req.params.id } }).then(gardener => {
+        const mapData = {
+            mapLocation: [gardener.latitude, gardener.longitude]
+        }
+        db.Garden.findAll().then(gardens => {
+            mapData.gardenPins = gardens.map(garden => {
+                gardenJSON = garden.toJSON();
+                return [gardenJSON.latitude, gardenJSON.longitude]
+            })
+            db.Compost.findAll().then(composts => {
+                mapData.compostPins = composts.map(compost => {
+                    compostJSON = compost.toJSON();
+                    return [compostJSON.latitude, compostJSON.longitude]
+                })
+                res.render("map", mapData)
+            }).catch(err => { res.status(500).json(err) })
+        }).catch(err => { res.status(500).json(err) })
+    }).catch(err => { res.status(500).json(err) })
+})
+
 module.exports = router;
