3fc0b6873edc61b5420349ffef1966f792cc48ac
Jae Kim <jaeshinkim321@gmail.com>
2020-10-21 12:25:31 -0700

    profiles load by owner id

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../models")
+const db = require("../models");
+const { route } = require("./ownerController");
 
 // HTML routes =================================
 // return index.handebars
 router.get("/", function (req, res) {
     res.render("index");
 });
+
 // return profile.handlebars
-router.get("/profile", function (req, res) {
+router.get("/profile", function(req,res){
     res.render("profile")
+});
+
+// return profile.handlebars by id
+router.get("/profile/:id", function (req, res) {
+    db.Owner.findOne({where: {id: req.params.id}}).then(result=>{
+        console.log(result.toJSON())
+        res.render("profile", result.toJSON())
+    }).catch(err=>{
+        res.status(500).send(err);
+    })
 });
 // return signup.handlebars for owners
 router.get("/owners/signup", function (req, res) {
