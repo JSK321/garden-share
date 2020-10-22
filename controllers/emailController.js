a64cd57da735416537bada06774e24148a99d699
ckomodo <chrissakwa@gmail.com>
2020-10-21 22:33:37 -0700

    updates

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -5,31 +5,37 @@ var nodemailer = require("nodemailer");
 
 // POST route to send email
 router.post("/email", function (req, res) {
+  console.log(req.user.email);
+  // The email that the sender typed in
+  const receivingEmail = req.body.receivingEmail;
   db.Owner.findOne({
     where: {
       id: req.body.userId,
-
     },
   }).then(function (data) {
     //email addresses
+    console.log(data);
     const newEmail = {
       from: "patchedapp@gmail.com", //can we get from DB using ID?
-      to: "chrissakwa@gmail.com", //can we get from DB using ID?
+      to: receivingEmail, //can we get from DB using ID?
       subject: "testing nodemailer",
       text: "this works as is",
     };
+    res.json(data);
+
+
     //email sending
-    transport.sendMail(newEmail, (err, info) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("email sent successfully" + info);
-      }
-      res.json(data);
-      // const userType = req.body.userType,
-      // const userId = req.body.userId,
-      // const emailBody = req.body.emailBody
-    });
+    // transport.sendMail(newEmail, (err, info) => {
+    //   if (err) {
+    //     console.log(err);
+    //   } else {
+    //     console.log("email sent successfully" + info);
+    //   }
+    //   res.json(data);
+    //   // const userType = req.body.userType,
+    //   // const userId = req.body.userId,
+    //   // const emailBody = req.body.emailBody
+    // });
   });
 });
 
