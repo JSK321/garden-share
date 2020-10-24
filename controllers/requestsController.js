9ea314be24b4faaf18f0f7b9bb0c716df687a79b
Elijah B <69051669+elijah415hz@users.noreply.github.com>
2020-10-24 12:40:32 -0700

    Working assign button

--- controllers/requestsController.js
+++ controllers/requestsController.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 
-router.post("/requests", async function(req, res){
+router.post("/requests", async function (req, res) {
     if (req.session.user && req.session.user.userType === "gardener") {
-        const gardener = await db.Gardener.findOne({where: {id: req.session.user.id}})
+        const gardener = await db.Gardener.findOne({ where: { id: req.session.user.id } })
         const gardenerId = gardener.toJSON().id;
         const gardenId = parseInt(req.body.gardenId);
         console.log({
@@ -18,6 +18,20 @@ router.post("/requests", async function(req, res){
         console.log(response);
         res.json(response)
     }
+})
+
+router.get("/requests/:id", function (req, res) {
+    db.Request.findAll({
+        where: { id: req.params.id },
+        include: [db.Garden, db.Gardener]
+        // include: db.Gardener
+    }).then(response=>{
+        if(response){
+            let responseJSON = response.map(element=>element.toJSON())
+            console.log(responseJSON)
+            res.json(responseJSON)
+        }
+    })
 })
 
 module.exports = router;
